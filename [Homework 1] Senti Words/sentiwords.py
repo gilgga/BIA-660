0087a176a4ed2aae33d1d61ee5ca7f1c527cc5d4
unknown <gaustria@stevens.edu>
2020-09-14 22:24:08 -0400

    Update header and clean up formatting

--- [Homework 1] Senti Words/sentiwords.py
+++ [Homework 1] Senti Words/sentiwords.py
@@ -1,13 +1,13 @@
 """
 * Name: sentiwords.py
-* Description: BIA-660-WS Homework 1
-* Authors: Group 8
+* Description: BIA-660-WS Homework 1 - Senti Words
+* Authors: Team 8: Gil Austria, Homa Deilamy, Korey Grabowski, Rashmi Swaroop, Victoria Piskarev
 * Pledge: "I pledge my honor that I have abided by the Stevens Honor System"
 """
 
 """
 Accepts a list of reviews and a Lexicon of positive words
-and returns the word that appears most frequently right after a positive word. 
+and returns the word that appears most frequently right after a positive word.
 """
 
 
@@ -22,23 +22,24 @@ def loadLexicon(fname):
 
     return newLex
 
+#function that reads in a file with reviews and a file with positive words
 #The function returns the word that appears most frequently right after a positive word.
 def run( reviews_file, positive_words_file  ):
     # Dictionary where
     #   Key = Word that appears after a positive word
     #   Value = The Number of times that word appears right after a positive word
-    words_and_counts_dict = {} 
-    
+    words_and_counts_dict = {}
+
     #load the positive lexicons
     posLex=loadLexicon(positive_words_file)
-    
+
     fin=open(reviews_file)  # Open text file
     for line in fin: # for every line in the file (1 review per line)
-        
+
         line=line.lower().strip()
-        
+
         words=line.split(' ') # slit on the space to get list of words
-   
+
         last_word_was_pos_flag = False  # Keeps track of if the previous word is in the positive words lexicon
         for word in words: #for every word in the review
             if last_word_was_pos_flag:  # If the last word is in the positive lexicon, then add the current word to the dictionary
@@ -56,22 +57,16 @@ def run( reviews_file, positive_words_file  ):
 
     # print(words_and_counts_dict)
 
-    max_count = -1  # Keeps track of the current max times a word has appeared
-    most_frequent_word = "" # Keeps track of the word that has appeared the most
+    max_count = -1  # Keeps track of the current max value for the frequency with which a word appeared after a positive word
+    most_frequent_word = "" # Keeps track of the word that has appeared the most after a positive word
     for (word_after_pos, count) in words_and_counts_dict.items():
         if count > max_count:
             max_count = count
             most_frequent_word = word_after_pos
-        
+
     return most_frequent_word   # Return the word that appears most frequently right after a positive word
 
 
-if __name__ == "__main__": 
-    most_frequent_word = run('textfile2', 'positive-words.txt')
+if __name__ == "__main__":
+    most_frequent_word = run('testfiles/textfile', 'lexicon/positive-words.txt')
     print(most_frequent_word)
-       
-
-
-
-
-
