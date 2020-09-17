25260f4799bde990d759895d3efa4698cab631ac
unknown <gaustria@stevens.edu>
2020-09-17 18:23:54 -0400

    Base File for Homework 2

---
+++ [Homework 2] Web Counter/webcounter.py
@@ -0,0 +1,19 @@
+"""
+* Name: webcounter.py
+* Description: BIA-660-WS Homework 2 - Web Counter
+* Authors: Team 8: Gil Austria, Homa Deilamy, Korey Grabowski, Rashmi Swaroop, Victoria Piskarev
+* Pledge: "I pledge my honor that I have abided by the Stevens Honor System"
+"""
+
+import re  # Regular Expressions for searching specific patterns
+from nltk.corpus import stopwords  # Popular library for text mining
+import requests  # HTTP Requests
+from operator import itemgetter  # For sorting different types of lists for different criteria
+
+
+def run( link1, link2, link3 ):
+
+
+    return
+
+
