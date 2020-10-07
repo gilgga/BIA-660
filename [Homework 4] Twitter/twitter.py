727f36f6d593e4db38d276bef6b5042d327fc5c3
unknown <gaustria@stevens.edu>
2020-10-07 16:17:01 -0400

    Update Comments

--- [Homework 4] Twitter/twitter.py
+++ [Homework 4] Twitter/twitter.py
@@ -11,88 +11,110 @@ import csv
 import re
 
 
+"""
+* Accepts 2 parameters
+*   link1: The link to a Twitter profile
+*   link2: The link to another Twitter profile
+* Returns the SET of words that appear in the text of both tweet1 and tweet2
+"""
 def getTweets( link1, link2 ):
+    # Windows-formatted ChromeDriver exe
     driver = webdriver.Chrome('chromedriver.exe')
 
     # *** Get the first 30 Tweets with Comment Counts from Link 1
+    # Open link1
     driver.get( link1 )
     time.sleep(2)
 
-    link1_already_seen = set()  #keeps track of tweets we have already seen.
+    link1_already_seen = set()  # Keeps track of tweets we have already seen.
+    # Dictionary that
+    #   Keeps the tweet_index as key
+    #   Keeps a nested dictionary with the format {'tweet': '', 'comments': X} as value
     link1_tweets_and_commentsCount = {}
-    link1_tweet_index = 0
+    link1_tweet_index = 0   # Keeps track of how many tweets have been added to the dictionary
 
-    # Store Link 1 Tweets and Comments Counts in Dictionary
+    # Store First 30 Link 1 Tweets and Comments Counts in Dictionary
     while( link1_tweet_index < 30 ):
 
         # Find all elements that have the value "tweet" for the data-testid attribute
         tweets=driver.find_elements_by_css_selector('div[data-testid="tweet"]')#
         # print(len(tweets),' tweets found\n')
 
-
         # Process each tweet
         for tweet in tweets:
 
             if tweet in link1_already_seen:
-                continue #we have seen this tweet before while scrolling down, ignore
-            link1_already_seen.add(tweet) #first time we see this tweet. Mark as seen and process.
+                continue # We have seen this tweet before while scrolling down, ignore
+            link1_already_seen.add(tweet) # First time we see this tweet. Mark as seen and process.
 
-            txt, comments = 'NA', 'NA'
+            txt, comments = 'NA', 'NA'  # Holds current tweet text and comment counts
 
-            # ** Find the Tweet Text
+            # Find the Tweet Text
             try:
                 txt = tweet.find_element_by_css_selector("div.css-901oao.r-jwli3a.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0").text
                 txt = txt.replace('\n', ' ')
             except:
                 print ('no text')
 
-            # ** Find the number of comments
+            # Find the number of comments
             try:
 
-                #find the div element that havs the value "retweet" for the data-testid attribute
-                retweetElement = tweet.find_element_by_css_selector('div[data-testid="reply"]')
+                # Find the div element that havs the value "reply" for the data-testid attribute
+                replyElement = tweet.find_element_by_css_selector('div[data-testid="reply"]')
 
-                #find the span element that has all the specified values (space separated) in its class attribute
-                comments = retweetElement.find_element_by_css_selector('span.css-901oao.css-16my406.r-1qd0xha.r-ad9z0x.r-bcqeeo.r-qvutc0').text
+                # Find the span element that has all the specified values (space separated) in its class attribute
+                comments = replyElement.find_element_by_css_selector('span.css-901oao.css-16my406.r-1qd0xha.r-ad9z0x.r-bcqeeo.r-qvutc0').text
 
             except:
                 print ('no comments')
 
-            #only write tweets that have text or retweets (or both).
+            # Only add tweets to dictionary that have text or comments (or both).
             if txt != 'NA' or comments != 'NA':
+                # Only add tweets to dictionary if there are less than 30 tweets in the dictionary
                 if ( link1_tweet_index < 30 ):
                     link1_tweet_index = link1_tweet_index + 1
+                    # Convert string representation of comments count to proper floats
                     if ( comments.endswith('K') ):
                         comments = float(comments.rstrip('K')) * 1000
                     elif ( comments.endswith('M') ):
                         comments = float(comments.rstrip('M')) * 1000000
+                    # Add tweet and comment count to dictionary
                     link1_tweets_and_commentsCount[link1_tweet_index] = {'tweet': txt, 'comments': int(comments)}
                 else:
                     break
 
+        # If there are less than 30 tweets in the Dictionary,
+        #   Scroll down twice to load more tweets
+        # Else
+        #   Stop scrolling and stop finding new tweets
         if ( link1_tweet_index < 30 ):
-            #scroll down twice to load more tweets
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         else:
             break
 
+        # Wait for 2 seconds
         time.sleep(2)
 
     print(link1_tweets_and_commentsCount)
     print('\n')
 
+
     # *** Get the first 30 Tweets with Like Counts from Link 2
+    # Open link2
     driver.get( link2 )
     time.sleep(2)
 
-    link2_already_seen = set()#keeps track of tweets we have already seen.
+    link2_already_seen = set()  # Keeps track of tweets we have already seen
+    # Dictionary that
+    #   Keeps the tweet_index as key
+    #   Keeps a nested dictionary with the format {'tweet': '', 'likes': X} as value
     link2_tweets_and_likesCount = {}
-    link2_tweet_index = 0
+    link2_tweet_index = 0   # Keeps track of how many tweets have been added to the dictionary
 
-    # Store Link 1 Tweets and Comments Counts in Dictionary
+    # Store Link 2 Tweets and Likes Counts in Dictionary
     while( link2_tweet_index < 30 ):
 
-        #find all elements that have the value "tweet" for the data-testid attribute
+        # Find all elements that have the value "tweet" for the data-testid attribute
         link2_tweets=driver.find_elements_by_css_selector('div[data-testid="tweet"]')#
         # print(len(link2_tweets),' tweets found\n')
 
@@ -100,48 +122,55 @@ def getTweets( link1, link2 ):
         for tweet in link2_tweets:
 
             if tweet in link2_already_seen:
-                continue #we have seen this tweet before while scrolling down, ignore
-            link2_already_seen.add(tweet) #first time we see this tweet. Mark as seen and process.
+                continue # We have seen this tweet before while scrolling down, ignore
+            link2_already_seen.add(tweet) # First time we see this tweet. Mark as seen and process.
 
-            txt, likes = 'NA', 'NA'
+            txt, likes = 'NA', 'NA' # Holds current tweet text and like counts
 
-            # ** Find the Tweet Text
+            # Find the Tweet Text
             try:
                 txt = tweet.find_element_by_css_selector("div.css-901oao.r-jwli3a.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0").text
                 txt = txt.replace('\n', ' ')
             except:
                 print ('no text')
 
-            # ** Find the number of comments
+            # Find the number of likes
             try:
 
-                #find the div element that havs the value "retweet" for the data-testid attribute
-                retweetElement = tweet.find_element_by_css_selector('div[data-testid="like"]')
+                # Find the div element that have the value "like" for the data-testid attribute
+                likeElement = tweet.find_element_by_css_selector('div[data-testid="like"]')
 
-                #find the span element that has all the specified values (space separated) in its class attribute
-                likes = retweetElement.find_element_by_css_selector('span.css-901oao.css-16my406.r-1qd0xha.r-ad9z0x.r-bcqeeo.r-qvutc0').text
+                # Find the span element that has all the specified values (space separated) in its class attribute
+                likes = likeElement.find_element_by_css_selector('span.css-901oao.css-16my406.r-1qd0xha.r-ad9z0x.r-bcqeeo.r-qvutc0').text
 
             except:
                 print ('no likes')
 
-            #only write tweets that have text or retweets (or both).
+            # Only add tweets to dictionary that have text or likes (or both).
             if txt != 'NA' or likes != 'NA':
+                # Only add tweets to dictionary if there are less than 30 tweets in the dictionary
                 if ( link2_tweet_index < 30 ):
                     link2_tweet_index = link2_tweet_index + 1
+                    # Convert string representation of likes count to proper floats
                     if ( likes.endswith('K') ):
                         likes = float(likes.rstrip('K')) * 1000
                     elif ( likes.endswith('M') ):
                         likes = float(likes.rstrip('M')) * 1000000
+                    # Add tweet and like count to dictionary
                     link2_tweets_and_likesCount[link2_tweet_index] = {'tweet': txt, 'likes': int(likes)}
                 else:
                     break
 
+        # If there are less than 30 tweets in the Dictionary,
+        #   Scroll down twice to load more tweets
+        # Else
+        #   Stop scrolling and stop finding new tweets
         if ( link2_tweet_index < 30 ):
-            #scroll down twice to load more tweets
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         else:
             break
 
+        # Wait for 2 seconds
         time.sleep(2)
 
     print(link2_tweets_and_likesCount)
@@ -170,6 +199,7 @@ def getTweets( link1, link2 ):
     print(tweet2)
     print(currentMaxLikes)
 
+
     # *** Process and Clean Up Tweet Text
     # Remove all non-alpha characters and lowercase tweet1
     tweet1 = re.sub( '[^a-z]', ' ', tweet1.lower() ) # Replace all non-letter characters with a space; Lowercase all letter characters
@@ -180,6 +210,7 @@ def getTweets( link1, link2 ):
     tweet2 = re.sub( '[^a-z]', ' ', tweet2.lower() ) # Replace all non-letter characters with a space; Lowercase all letter characters
     tweet2_words = tweet2.split(' ') # Split to get the words in the text
     print(tweet2_words)
+
 
     # *** Create set of words from Tweet Text
     # Create set for tweet1
@@ -194,10 +225,13 @@ def getTweets( link1, link2 ):
         tweet2_set.add(word)
     print(tweet2_set)
 
+
     # *** Create the set of words that appear in the text of both tweet1 and tweet2
     tweet1_and_tweet2_set = tweet1_set.intersection(tweet2_set)
 
+
     # *** Return set of words that appear in the text of both tweet1 and tweet2
     return tweet1_and_tweet2_set
+
 
 getTweets('https://twitter.com/SHAQ', 'https://twitter.com/DwyaneWade')
