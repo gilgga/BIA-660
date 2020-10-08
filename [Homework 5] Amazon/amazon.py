a084ef37d6b41275a0ab7362117bd2801d996cb3
unknown <gaustria@stevens.edu>
2020-10-08 18:09:37 -0400

    Base file for final submission

---
+++ [Homework 5] Amazon/amazon.py
@@ -0,0 +1,11 @@
+"""
+* Name: amazon.py
+* Description: BIA-660-WS Homework 4 - Amazon
+* Authors: Team 8: Gil Austria, Homa Deilamy, Korey Grabowski, Rashmi Swaroop, Victoria Piskarev
+* Pledge: "I pledge my honor that I have abided by the Stevens Honor System"
+"""
+
+def scrape():
+
+    return
+
